9e69ca674a70687fc09cfc32c3fe73d212277682
SMilena <sandramilenagc8@gmail.com>
2021-07-11 20:16:51 -0500

    Responsive y css

--- src/main.js
+++ src/main.js
@@ -1,37 +1,71 @@
 import data from "./data/data.js";
 const dataUsers = Object.values(data);
-//console.log('datausers', dataUsers)
 
-const dataUrl = dataUsers.map((dataUsers) =>
-  fetch(dataUsers.url)
-    .then((response) => response.json())
-    .then((dato) => {
-      //console.log('datooo', dato)
-      return dato;
-    })
-);
+//Utilizar token para acceder a la API de GitHub | Promesa con peticion fetch que resuelve la data de la API
+const dataUrl = (dataElement) => {
+  return new Promise((resolve) => {
+    let myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      "Basic U01pbGVuYUdyYW5hZG9zOmdocF9QUUxMclVlVFdkR3lIaG9FWDVGN2o0dTRnUE55Z1AwNElNVkc="
+    );
 
-const showUsers = (data, dato) => {
-  const mainContainer = document.getElementById("applicationUsers");
-  if (mainContainer.hasChildNodes()) {
-    while (mainContainer.childNodes.length >= 1) {
-      mainContainer.removeChild(mainContainer.firstChild);
-    }
-  }
-  for (let i = 0; i < data.length; i++) {
-    let div = document.createElement("div");
-    div.innerHTML = `
-      
-      <div id="card-body">
-          <div class="card-image">
-          <img src="${data[i].avatar_url}" id="img">
-          <h2 class="card-title">User: ${data[i].login}</h2>
-          <h3 class="card-title">${data[i].html_url}</h3>
-          </div>
-      </div>`;
-
-    mainContainer.appendChild(div);
-  }
+    let requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    fetch(dataElement.url, requestOptions)
+      .then((response) => response.json())
+      .then((dato) => {
+        resolve(dato);
+      })
+      .catch((e) => console.error("errorr " + e));
+  });
 };
-showUsers(dataUsers, dataUrl);
 
+//Función mostrar usuarios y data
+const showUsers = (data) => {
+  const mainContainer = document.getElementById("applicationUsers");
+  const completeData = data.map((item) => dataUrl(item));
+  Promise.all(completeData).then((res) => {
+    if (mainContainer.hasChildNodes()) {
+      while (mainContainer.childNodes.length >= 1) {
+        mainContainer.removeChild(mainContainer.firstChild);
+      }
+    }
+    for (let i = 0; i < res.length; i++) {
+      let div = document.createElement("div");
+      div.innerHTML = `
+        
+        <div id="card-body">
+            <div class="card-image">
+              <p id="title">User : ${res[i].login}</p>
+              <p id="textLink">${res[i].html_url}</p>
+              <img src="${res[i].avatar_url}" id="img">
+              <p id="text-name">Name : ${res[i].name}</p>
+              
+              <div id="text-card">
+              <p>Company : ${res[i].company}</p>
+              <p>Blog : ${res[i].blog}</p>
+              <p>Location : ${res[i].location}</p>
+              <p>e-mail : ${res[i].email}</p>
+              <p>About-me : ${res[i].bio}</p>
+              <p>Twitter : ${res[i].twitter_username}</p>
+              </div>
+
+              <div id="text-numbers">
+              <p>Repositories : ${res[i].public_repos}</p>
+              <p>Gists : ${res[i].public_gists}</p>
+              <p>Followers : ${res[i].followers}</p>
+              <p>Following : ${res[i].following}</p>
+              </div>
+              
+            </div>
+        </div>`;
+
+      mainContainer.appendChild(div);
+    }
+  });
+};
+showUsers(dataUsers);
